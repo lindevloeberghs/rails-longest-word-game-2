3d3b93076aee46e7b0e09c7f6d6c3117b01af7ef
Linde Vloeberghs <lindevloeberghs@me.com>
2017-03-20 20:01:49 +0100

    livecode recap of wk6-1 rails basics lecture and exercises at lewagon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'game', to: 'stupid_game#game'
+  get 'score', to: 'stupid_game#score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
